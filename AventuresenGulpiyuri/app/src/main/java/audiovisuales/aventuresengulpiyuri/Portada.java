7d456d0655983656545d94bbc6ba96ae6f9107c7
meri294 <merigg94@gmail.com>
2015-12-12 18:58:18 +0100

    Páginas de cuento acabadas. Solucionado problema con XML

--- AventuresenGulpiyuri/app/src/main/java/audiovisuales/aventuresengulpiyuri/Portada.java
+++ AventuresenGulpiyuri/app/src/main/java/audiovisuales/aventuresengulpiyuri/Portada.java
@@ -18,19 +18,7 @@ public class Portada extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-       Utilidades.mostrarVentanaLecturaAutomatica(this);
+        Utilidades.mostrarVentanaLecturaAutomatica(this);
     }
 
     public void pasaPagina(View view){
